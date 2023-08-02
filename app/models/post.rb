c10120d9d7d3a6890216096b7f8c1d6eaaa1301f
“rahulgupta34” <rahul.g@simformsolutions.com>
2023-08-02 14:52:36 +0530

    Post Role Change Using Admin Portal [02/08/2023] enable dynamic find methods for roles like is_admin?,is_editor? Etc add adminposts resource in routers.rb file create adminposts controller and it’s index,edit and update action, corresponding views

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
     has_one_attached :avatar
     resourcify
     validates :title, presence: true
-    has_many :users, through: :roles, source: :User, class_name: "User", source: :users
+    has_many :users, through: :roles, class_name: "User", source: :users
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :admins
     resources :adminusers, only: [:index]
+    resources :adminposts, only: [:index]
   end
       root "users#index"
       resources :users
       get "users/adminusersupdate/:id", to: "admin/adminusers#edit"
       patch "users/admin/:id", to: "admin/adminusers#update"
+
+      get "admin/adminpostsroleedit/:id", to: "admin/adminposts#edit"
+      patch "admin/adminpostsupdaterole/:id", to: "admin/adminposts#update"
 end
